1d99280465d0bbec358973aca012cf0422c5f245
Sean Hogan <hoagies@Seans-MacBook-Pro.local>
2017-11-30 23:24:35 -0500

    Updated for Test Kitchen

--- cookbooks/testbook/recipes/default.rb
+++ cookbooks/testbook/recipes/default.rb
@@ -3,3 +3,16 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+# http://inspec.io/docs/reference/resources/package/
+package "apache2" do
+  action :install
+end
+
+service "apache2" do
+  action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+end
